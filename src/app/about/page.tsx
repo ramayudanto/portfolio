0b1c580a8c7f92990ad160644b22b450f7530c8a
ramayudanto <ramayudanto@gmail.com>
2024-07-10 22:05:11 +0700

    1st commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { metadata } from '../layout'
+metadata.title = 'About'
+metadata.description = 'About page'
+metadata.keywords = ['about', 'page']
+
+
+export default function page() {
+  return (
+    <div>
+      <p>about</p>
+    </div>
+  )
+}
