ec13c61af6d0ec2bdd3f96dba005019f82371728
ramayudanto <ramayudanto@gmail.com>
2024-07-13 15:52:55 +0700

    add copy email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,41 @@
 "use client";
 
-import React from 'react';
-import { Dribbble, Github, Twitter } from "iconoir-react";
+import React, { useState } from "react";
+import { ArrowEmailForward, Dribbble, Github, Mail, Twitter } from "iconoir-react";
 
 const metadata = {
   title: "Home",
-  description: "serves as designer on the labs, I design and build digital products that are easy to use and look great. I'm a designer/maker and I'm passionate about creating delightful user experiences."
+  description:
+    "serves as designer on the labs, I design and build digital products that are easy to use and look great. I'm a designer/maker and I'm passionate about creating delightful user experiences.",
 };
 
 const socials = [
   {
     name: "Dribbble",
     link: "https://dribbble.com/ramayudanto",
-    icon: <Dribbble color="neutral-500" stroke="1px" height={20} width={20} />
+    icon: <Dribbble color="neutral-500" stroke="1px" height={20} width={20} />,
   },
   {
     name: "Twitter",
     link: "https://twitter.com/ramayudanto",
-    icon: <Twitter color="neutral-500" stroke="1px" height={20} width={20} />
+    icon: <Twitter color="neutral-500" stroke="1px" height={20} width={20} />,
   },
   {
     name: "Github",
     link: "https://github.com/ramayudanto",
-    icon: <Github color="neutral-500" stroke="1px" height={20} width={20} />
-  }
+    icon: <Github color="neutral-500" stroke="1px" height={20} width={20} />,
+  },
 ];
 
 export default function Home() {
+  const [buttonText, setButtonText] = useState("Copy Email");
+
   const copyEmail = () => {
-    navigator.clipboard.writeText("hi@example.com");
+    navigator.clipboard.writeText("hi@ramayudanto.com");
+    setButtonText("Email Copied");
+    setTimeout(() => {
+      setButtonText("Copy Email");
+    }, 3000);
   };
 
   return (
@@ -49,12 +56,14 @@ export default function Home() {
           </a>
         ))}
       </div>
-      {/* <button
-          onClick={copyEmail}
-          className="flex gap-3 p-3  hover:bg-neutral-700 rounded-lg text-neutral-500 stroke-neutral-500 hover:stroke-gray-100 hover:text-gray-100"
-        >
-          Copy Email
-        </button> */}
+      <button
+        onClick={copyEmail}
+        className="mt-4 flex  items-center gap-2 px-3 py-2 bg-white hover:bg-neutral-100 rounded-lg text-neutral-500 stroke-neutral-500 hover:stroke-gray-100"
+      >
+        <Mail color="neutral-500" stroke="1px" height={20} width={20} />
+        {buttonText}
+      </button>
+      
     </main>
   );
 }
